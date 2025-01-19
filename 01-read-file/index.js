64440661e1a9d8a8fb63ed118fcda9c6c0c52ccf
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-01-19 12:37:39 +0100

    refactor: rename variable steam to stream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,9 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const pathToFile = path.join(__dirname, 'text.txt');
-const steam = fs.createReadStream(pathToFile, { encoding: 'utf-8' });
+const stream = fs.createReadStream(pathToFile, { encoding: 'utf-8' });
 
-steam.on('data', (chunk) => {
+stream.on('data', (chunk) => {
   process.stdout.write(chunk);
 });
-steam.on('error', (err) => console.log('Error', err.message));
+stream.on('error', (err) => console.log('Error', err.message));
