c335a23aa0bf121fb2917fcf388925d60abe3b4b
Anastasiia Barkovskaia <tosigaeva@gmail.com>
2025-01-20 21:46:00 +0100

    fix: ensure farewell message displays on Ctrl+C in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,19 +7,23 @@ const stream = fs.createWriteStream(filePath, { flags: 'a' });
 
 const rl = readline.createInterface({
   input: process.stdin,
+  output: process.stdout,
 });
 
 console.log('Please enter some text. Type "exit" or press Ctr+C to quit.');
 
 rl.on('line', (input) => {
   if (input === 'exit') {
+    console.log('Goodbye!');
     rl.close();
+    process.exit(0);
   } else {
     stream.write(`${input}\n`);
   }
 });
 
 rl.on('SIGINT', () => {
+  console.log('Goodbye!');
   rl.close();
+  process.exit(0);
 });
-
